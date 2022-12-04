37fe4bd9f9166d0297e65c72a433672c3d829904
Colin Poon <me@colinpoon.com>
2022-12-04 15:09:03 -0800

    tailwind test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import './App.css'
+import './index.css'
 import React from 'react';
-
 import Hello from './Hello';
 
 function App() {
   return (
+    // <div className='App flex-col bg-red-300'>
     <>
       <Hello />
     </>
+    // </div>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -4,15 +4,20 @@ const Hello = () => {
 
 
   return (
-    <>
-    <div>
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname"></input>
-  
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname"></input>
-      </div>
-    </>
+    <div className='App flex items-center justify-center h-screen w-screen bg-dark'>
+      <>
+      <div className='container-card flex-col items-center gap-4'>
+        <div className='flex flex-col w-50'>
+          <label >First Name:</label>
+          <input type="text" id="fname" className='form-text' name="fname"></input>
+        </div>
+        <div className='flex flex-col'>
+          <label >Last name:</label>
+          <input type="text" id="fname" className='form-text' name="fname"></input>
+        </div>
+        </div>
+      </>
+    </div>
   )
 }
 
