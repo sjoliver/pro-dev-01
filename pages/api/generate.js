a2da8877dd232ce1fab5b1ad9db1a25f7aa6ae58
Colin Poon <me@colinpoon.com>
2022-12-18 16:43:54 -0800

    changing ai learning data

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -20,9 +20,9 @@ function generatePrompt(animal) {
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Names: Agent Meow,  Neil Catrick, The Incredible Cat Benatar
 Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Names: Mister Pickles, Ally McBeagle, JK Growling
 Animal: ${capitalizedAnimal}
 Names:`;
 }
